b1306626bf14df376b37129bc1c66fa0f125ab24
mmaceli <mmaceli@pratt.edu>
2019-01-30 17:50:48 -0500

    Added week 2 file

---
+++ week2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Week 2 Page</title>
+</head>
+<body>
+<h1>Exercises</h1>
+<?php
+# Arithmetic operators
+
+$first = "Monica";
+$middle = "G";
+
+print $middle.", ".$first;
+
+$age = 40;
+$in5 = $age + 5;
+$difference = (2034-2019)+$age;
+$months = $age*12;
+print ++$age;
+
+?>
+</body>
+</html>
+
+
+
+
+
+
+
+
